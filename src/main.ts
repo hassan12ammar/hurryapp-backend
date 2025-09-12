1fe75a118c5d33f3ba13287c4ec9a9dfa8239461
hassan12ammar <mgamea125@gmail.com>
2025-09-12 21:56:26 +0300

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,44 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { env } from './libs/env'
 import { Logger } from '@nestjs/common'
+import { patchNestjsSwagger } from '@anatine/zod-nestjs'
+import { ZodValidationPipe } from './libs/zod'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { writeFileSync } from 'fs'
+import { apiReference } from '@scalar/nestjs-api-reference'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   })
-  await app.listen(env.PORT)
 
-  Logger.log(
-    `Application is running on: http://localhost:${env.PORT}`,
-    'Bootstrap',
+  patchNestjsSwagger()
+
+  let optionsBuilder = new DocumentBuilder().setTitle('fingerPrint')
+  optionsBuilder = optionsBuilder.setDescription('here is the description')
+
+  app.useGlobalPipes(new ZodValidationPipe())
+
+  const document = SwaggerModule.createDocument(app, optionsBuilder.build())
+
+  // write openapi.json file, for debugging
+  writeFileSync('openapi.json', JSON.stringify(document, null, 2))
+
+  app.use(
+    '/docs',
+    apiReference({
+      spec: { content: document },
+      theme: 'saturn',
+      layout: 'modern',
+    }),
   )
+
+  await app.listen(env.PORT, () => {
+    Logger.log(
+      `Application is running on: http://localhost:${env.PORT}`,
+      'Bootstrap',
+    )
+  })
 }
 
 bootstrap()
